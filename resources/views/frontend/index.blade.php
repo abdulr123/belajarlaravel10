da06de436a4c53aced9dd647dde8e13d837597be
Abdul Rahman <tugas.abdulrahman@gmail.com>
2024-01-30 08:23:41 +0700

    add kategori

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,18 @@
+<x-app-web-layout>
+
+    <x-slot:title>
+        Belajar Laravel 10 - Abdul Rahman
+    </x-slot>
+
+
+    <div class="py-5">
+        <div class="container">
+            <h4>Halaman awal</h4>
+        </div>
+    </div>
+
+    <x-slot name="scripts">
+        <!-- Your script code -->
+    </x-slot>
+
+</x-app-layout>
